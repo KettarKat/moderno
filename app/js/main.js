c11e271297b2872c3e6175e2c0808b3d980292e3
KettarKat <kettarkat@gmail.com>
2020-06-01 19:17:49 +0300

    ended header and footer adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -33,6 +33,17 @@ $('.icon-th-large').on('click', function(){
   $('.icon-list').removeClass('active');
 });
 
+
+  $('.menu__btn ').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
+
   var mixer = mixitup('.products__inner-box');
 
 
