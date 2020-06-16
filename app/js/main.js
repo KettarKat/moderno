c18b08d62344416dc531bc5849898818a535e56d
KettarKat <kettarkat@gmail.com>
2020-06-16 19:50:51 +0300

    added personal settings page

--- app/js/main.js
+++ app/js/main.js
@@ -14,12 +14,11 @@ $(function(){
 
   $(".js-range-slider").ionRangeSlider({
     type: "double",
-        
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
 });
 
 $('.icon-list').on('click', function(){
@@ -43,10 +42,10 @@ $('.icon-th-large').on('click', function(){
     $('.header__box').toggleClass('active');
   });
 
-  $('.product-one__tabs .tab').on('click', function(event) {
+  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
     var id = $(this).attr('data-id');
-      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
       $(this).addClass('active');
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
